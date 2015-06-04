655ca3c385868d23dc989dbab72289f2cb9da7f6
Nezhdetov <gyokan.nezhdetov@gmail.com>
2015-06-04 20:03:56 +0300

    Exam

---
+++ 00.Exam-Preparation/Exam-26-April-2015-Evening/1.CompoundInterest/CompoundInterest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class CompoundInterest
+{
+    static void Main()
+    {
+        double tvPrice = double.Parse(Console.ReadLine());
+        int bankLoanTerm = int.Parse(Console.ReadLine());
+        double bankInterest = double.Parse(Console.ReadLine());
+        double friendInterest = double.Parse(Console.ReadLine());
+
+        double bankAmount = tvPrice * Math.Pow((1 + bankInterest), bankLoanTerm);
+        double friendAmount = tvPrice * (1 + friendInterest);
+        if (bankAmount < friendAmount)
+        {
+            Console.WriteLine("{0:F2} Bank", bankAmount);
+        }
+        else
+        {
+            Console.WriteLine("{0:F2} Friend", friendAmount);
+        }
+    }
+}
