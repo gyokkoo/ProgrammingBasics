d4be5ef0461cc7481908fd98e2be38e13d58b98c
Nezhdetov <Nezhdetov@users.noreply.github.com>
2015-06-18 18:05:37 +0300

    Op

--- 05.Operators-Expressions-And-Statements-Homework/05.ThirdDigitIsSeven/ThirdDigitIsSeven.cs
+++ 05.Operators-Expressions-And-Statements-Homework/05.ThirdDigitIsSeven/ThirdDigitIsSeven.cs
@@ -18,8 +18,7 @@ class ThirdDigitIsSeven
     {
         Console.WriteLine("Enter an integer n to see if its third digit from right-to-left is 7");
         int integerN = int.Parse(Console.ReadLine());
-        int thirdDigit = (integerN / 100) % 10;
-        bool thirdDigitSeven = thirdDigit == 7 ? true : false;
-        Console.WriteLine(thirdDigitSeven);
+        bool thirdDigit = (integerN / 100) % 10 == 7;
+        Console.WriteLine(thirdDigit);
     }
 }
