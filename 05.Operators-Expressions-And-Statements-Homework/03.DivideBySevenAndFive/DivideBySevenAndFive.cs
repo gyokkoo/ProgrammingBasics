ac20095986b11ea157ce93f55ce0ad5ad918c52d
Nezhdetov <Nezhdetov@users.noreply.github.com>
2015-06-18 17:38:47 +0300

    Optimized
    In more simply way.

--- 05.Operators-Expressions-And-Statements-Homework/03.DivideBySevenAndFive/DivideBySevenAndFive.cs
+++ 05.Operators-Expressions-And-Statements-Homework/03.DivideBySevenAndFive/DivideBySevenAndFive.cs
@@ -15,11 +15,9 @@ class DivideBySevenAndFive
 {
     static void Main()
     {
-        Console.WriteLine("Please enter integer to see if it can be divided by 7 and 5 in the same time");
-        int integerN = int.Parse(Console.ReadLine());
-        bool dividedBySeven = (integerN % 7 == 0 ? true : false);
-        bool dividedByFive = (integerN % 5 == 0 ? true : false);
-        bool moreThan = (integerN >= 35 ? true : false);
-        Console.WriteLine(dividedByFive && dividedByFive && moreThan);
+            Console.WriteLine("Please enter integer to see if it can be divided by 7 and 5 in the same time");
+            int integerN = int.Parse(Console.ReadLine());
+            bool isDivided = (integerN % 7 == 0 && integerN % 5 == 0);
+            Console.WriteLine(isDivided);
     }
 }
