e3efd4a9a7b9237d428d8dbbae2cb0195d999a25
Nezhdetov <gyokan.nezhdetov@gmail.com>
2015-06-30 20:07:57 +0300

    Exam update
    new tasks

---
+++ 00.Exam-Preparation/Exam-10-April-2014-Evening/4.HayvanNumbers/HayvanNumbers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class HayvanNumbers
+{
+    static void Main()
+    {
+        int sum = int.Parse(Console.ReadLine());
+        int diff = int.Parse(Console.ReadLine());
+        int abc;
+        int def;
+        int ghi;
+        bool hayvanNumber = true;
+        for (int a = 5; a <= 9; a++)
+        {
+            for (int b = 5; b <= 9; b++)
+            {
+                for (int c = 5; c <= 9; c++)
+                {
+                    abc = a * 100 + b * 10 + c;
+                    for (int d = 5; d <= 9; d++)
+                    {
+                        for (int e = 5; e <= 9; e++)
+                        {
+                            for (int f = 5; f <= 9; f++)
+                            {
+                                def = d * 100 + e * 10 + f;
+                                for (int g = 5; g <= 9; g++)
+                                {
+                                    for (int h = 5; h <= 9; h++)
+                                    {
+                                        for (int i = 5; i <= 9; i++)
+                                        {
+                                            ghi = g * 100 + h * 10 + i;
+                                            if ((sum == a + b + c + d + e + f + g + h + i) && (diff == ghi - def) && (diff == def - abc) && (abc < def) && (def < ghi))
+                                            {
+                                                Console.WriteLine(abc * 1000000 + def * 1000 + ghi);
+                                                hayvanNumber = false;
+                                            }
+
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (hayvanNumber)
+        {
+            Console.WriteLine("No");
+        }
+    }
+}
--- 00.Exam-Preparation/Exam-10-April-2014-Evening/5.BitsUp/BitsUp.cs
+++ 00.Exam-Preparation/Exam-10-April-2014-Evening/5.BitsUp/BitsUp.cs
@@ -5,27 +5,20 @@ class Program
     {
         int n = int.Parse(Console.ReadLine());
         int step = int.Parse(Console.ReadLine());
-        int mask = 0;
-        int result = 0;
+        int index = 0;
         int[] array = new int[n];
         for (int i = 0; i < n; i++)
         {
-            int number = int.Parse(Console.ReadLine()); // 01101101 
-           // Console.WriteLine(Convert.ToString(number, 2).PadLeft(8, '0'));
-                    
-            mask = 1 << 3;
-            result = number | mask;
-            array [n] = result;
-               // Console.WriteLine(Convert.ToString(result, 2).PadLeft(8, '0'));         
+            int number = int.Parse(Console.ReadLine());
+            for (int bit = 7; bit >= 0; bit--)
+            {
+                if ((index % step == 1) || (step == 1 && index > 0))
+                {
+                    number = number | (1 << bit);
+                }
+                index++;
+            }
+            Console.WriteLine(number);
         }
-     
-
-        //int n = int.Parse(Console.ReadLine());
-        //int step = int.Parse(Console.ReadLine());
-        //Console.WriteLine(Convert.ToString(n, 2).PadLeft(8, '0'));
-        //int mask = 1 >> step;
-        //int result = n | mask;
-        //Console.WriteLine(Convert.ToString(result, 2).PadLeft(8, '0'));
-        //Console.WriteLine(result);
     }
 }
