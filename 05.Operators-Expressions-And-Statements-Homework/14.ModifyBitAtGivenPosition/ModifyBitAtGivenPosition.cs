22af739fe5463ffd95e2f818908531e94fdb2397
Nezhdetov <Nezhdetov@users.noreply.github.com>
2015-07-03 11:42:45 +0300

    Update ModifyBitAtGivenPosition.cs

--- 05.Operators-Expressions-And-Statements-Homework/14.ModifyBitAtGivenPosition/ModifyBitAtGivenPosition.cs
+++ 05.Operators-Expressions-And-Statements-Homework/14.ModifyBitAtGivenPosition/ModifyBitAtGivenPosition.cs
@@ -2,7 +2,7 @@
 /*
 We are given an integer number n, a bit value v (v=0 or 1) and a position p. Write a sequence of operators (a few lines of C# code) that modifies n to hold the value v at the position p from the binary representation of n while preserving all other bits in n. Examples:
 n	binary representation of n	p	v	binary result	    result
-5	00000000 00000101    	    2	0	00000000 00000001	1
+5	00000000 00000101       	    2	0	00000000 00000001	1
 0	00000000 00000000        	9	1	00000010 00000000	512
 15	00000000 00001111        	1	1	00000000 00001111	15
  */
